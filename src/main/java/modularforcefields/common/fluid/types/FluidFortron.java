b0d4321837ae40adb0ecae0d99467e2063af44e1
aurilisdev <aurilisdev@gmail.com>
2022-01-14 00:28:18 +0100

    Fix fortron fluid color

--- src/main/java/modularforcefields/common/fluid/types/FluidFortron.java
+++ src/main/java/modularforcefields/common/fluid/types/FluidFortron.java
@@ -9,7 +9,7 @@ public class FluidFortron extends FluidNonPlaceable {
 	public static final String FORGE_TAG = "fortron";
 
 	public FluidFortron() {
-		super(() -> DeferredRegisters.ITEM_CANISTERREINFORCED, References.ID, "fortron", -428574419);
+		super(() -> DeferredRegisters.ITEM_CANISTERREINFORCED, References.ID, "fortron");
 	}
 
 }
