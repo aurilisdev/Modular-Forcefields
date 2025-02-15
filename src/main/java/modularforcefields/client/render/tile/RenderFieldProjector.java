9791657b498c4c167ecf6b0c1d3dd24816cfcb9f
aurilisdev <aurilisdev@gmail.com>
2025-02-15 21:50:06 +0100

    Add custom rendering fancy/fast to fabulous

--- src/main/java/modularforcefields/client/render/tile/RenderFieldProjector.java
+++ src/main/java/modularforcefields/client/render/tile/RenderFieldProjector.java
@@ -12,6 +12,7 @@ import electrodynamics.prefab.utilities.math.MathUtils;
 import modularforcefields.client.ClientRegister;
 import modularforcefields.common.tile.TileFortronFieldProjector;
 import modularforcefields.common.tile.projection.ProjectionType;
+import net.minecraft.client.GraphicsStatus;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -23,70 +24,76 @@ import net.minecraft.world.item.DyeColor;
 public class RenderFieldProjector extends AbstractTileRenderer<TileFortronFieldProjector> {
 
     public RenderFieldProjector(BlockEntityRendererProvider.Context context) {
-        super(context);
+	super(context);
     }
 
     @Override
-    public void render(TileFortronFieldProjector tile, float partialTicks, PoseStack poseStack, MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn) {
-        double ticks = tile.<ComponentTickable>getComponent(IComponentType.Tickable).getTicks();
-        float base = (float) ((ticks + partialTicks) * 1 % 360);
-        float rotX = base;
-        float rotY = base;
-        float rotZ = base;
+    public void render(TileFortronFieldProjector tile, float partialTicks, PoseStack poseStack,
+	    MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn) {
+	double ticks = tile.<ComponentTickable>getComponent(IComponentType.Tickable).getTicks();
+	float base = (float) ((ticks + partialTicks) * 1 % 360);
+	float rotX = base;
+	float rotY = base;
+	float rotZ = base;
 
-        DyeColor dyeColor = tile.getFieldColor();
+	DyeColor dyeColor = tile.getFieldColor();
 
-        Color color = new Color(dyeColor.getFireworkColor());
+	Color color = new Color(dyeColor.getFireworkColor());
 
-        BakedModel ibakedmodel = null;
-        if (tile.fortron.get() > 0) {
+	BakedModel ibakedmodel = null;
+	if (tile.fortron.get() > 0) {
 
 
-            poseStack.pushPose();
+	    BakedModel shape = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_FIELDFORTRON);
 
-            BakedModel shape = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_FIELDFORTRON);
+	    if (Minecraft.getInstance().options.graphicsMode().get() == GraphicsStatus.FABULOUS) {
+		RenderSystem.setShaderColor(color.rFloat(), color.gFloat(), color.bFloat(), 1);
+		poseStack.pushPose();
+		poseStack.translate(0.5, 0.5, 0.5);
+		RenderingUtils.renderModel(shape, tile, RenderType.translucent(), poseStack, bufferIn, combinedLightIn,
+			combinedOverlayIn);
+	    } else {
+		poseStack.pushPose();
+	    }
+	    for (BakedQuad quad : shape.getQuads(null, null, tile.getLevel().random)) {
+		bufferIn.getBuffer(RenderType.translucent()).putBulkData(poseStack.last(), quad, color.rFloat(),
+			color.gFloat(), color.bFloat(), 1.0F, combinedLightIn, combinedOverlayIn, true);
+	    }
 
-            //poseStack.translate(0.5, 0.5, 0.5);
+	    poseStack.popPose();
+	    switch (ProjectionType.values()[tile.typeOrdinal.get()]) {
+	    case HEMISPHERE:
+		ibakedmodel = Minecraft.getInstance().getModelManager()
+			.getModel(ClientRegister.MODEL_PREVIEWHALFSPHERE);
+		break;
+	    case PYRAMID:
+		ibakedmodel = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_PREVIEWPYRAMID);
+		break;
+	    case SPHERE:
+		ibakedmodel = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_PREVIEWSPHERE);
+		break;
+	    case CUBE:
+		ibakedmodel = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_PREVIEWCUBE);
+		break;
+	    default:
+	    case NONE:
+		break;
+	    }
+	    if (ibakedmodel != null) {
+		poseStack.translate(0.5, 1.25, 0.5);
+		poseStack.scale(0.25f, 0.25f, 0.25f);
+		poseStack.mulPose(MathUtils.rotQuaternionDeg(rotX, rotY, rotZ));
+		if (Minecraft.getInstance().options.graphicsMode().get() == GraphicsStatus.FABULOUS) {
+		    RenderingUtils.renderModel(ibakedmodel, tile, RenderType.translucent(), poseStack, bufferIn,
+			    combinedLightIn, combinedOverlayIn);
+		}
+		for (BakedQuad quad : ibakedmodel.getQuads(null, null, tile.getLevel().random)) {
+		    bufferIn.getBuffer(RenderType.translucent()).putBulkData(poseStack.last(), quad, color.rFloat(),
+			    color.gFloat(), color.bFloat(), 1.0F, combinedLightIn, combinedOverlayIn, true);
+		}
+	    }
 
-            //RenderingUtils.renderModel(shape, tile, RenderType.translucent(), poseStack, bufferIn, combinedLightIn, combinedOverlayIn);
-
-            //poseStack.popPose();
-
-            for (BakedQuad quad : shape.getQuads(null, null, tile.getLevel().random)) {
-
-                bufferIn.getBuffer(RenderType.translucent()).putBulkData(poseStack.last(), quad, color.rFloat(), color.gFloat(), color.bFloat(), 1.0F, combinedLightIn, combinedOverlayIn, true);
-
-            }
-
-            poseStack.popPose();
-            switch (ProjectionType.values()[tile.typeOrdinal.get()]) {
-                case HEMISPHERE:
-                    ibakedmodel = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_PREVIEWHALFSPHERE);
-                    break;
-                case PYRAMID:
-                    ibakedmodel = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_PREVIEWPYRAMID);
-                    break;
-                case SPHERE:
-                    ibakedmodel = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_PREVIEWSPHERE);
-                    break;
-                case CUBE:
-                    ibakedmodel = Minecraft.getInstance().getModelManager().getModel(ClientRegister.MODEL_PREVIEWCUBE);
-                    break;
-                default:
-                case NONE:
-                    break;
-            }
-            if (ibakedmodel != null) {
-                poseStack.translate(0.5, 1.25, 0.5);
-                poseStack.scale(0.25f, 0.25f, 0.25f);
-                poseStack.mulPose(MathUtils.rotQuaternionDeg(rotX, rotY, rotZ));
-                //RenderingUtils.renderModel(ibakedmodel, tile, RenderType.translucent(), poseStack, bufferIn, combinedLightIn, combinedOverlayIn);
-                for (BakedQuad quad : ibakedmodel.getQuads(null, null, tile.getLevel().random)) {
-                    bufferIn.getBuffer(RenderType.translucent()).putBulkData(poseStack.last(), quad, color.rFloat(), color.gFloat(), color.bFloat(), 1.0F, combinedLightIn, combinedOverlayIn, true);
-                }
-            }
-
-        }
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+	}
+	RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
