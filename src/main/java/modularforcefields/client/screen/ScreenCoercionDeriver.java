a430db8a5f56dcf1a9f5542e32fc61016d2f02c5
skip999 <skiprocks999@gmail.com>
2025-05-18 12:39:56 -0500

    data cleanup and bump version

--- src/main/java/modularforcefields/client/screen/ScreenCoercionDeriver.java
+++ src/main/java/modularforcefields/client/screen/ScreenCoercionDeriver.java
@@ -30,12 +30,17 @@ public class ScreenCoercionDeriver extends GenericScreen<ContainerCoercionDerive
         }, 8, 27));
         addComponent(new ScreenComponentElectricInfo(-AbstractScreenComponentInfo.SIZE + 1, 2).wattage(electro -> electro.getHolder() instanceof TileCoercionDeriver deriver ? (double) deriver.fortron.getValue() : 0));
         addComponent(new ScreenComponentMultiLabel(0, 0, poseStack -> {
-            if (menu.getUnsafeHost() instanceof TileCoercionDeriver deriver) {
-            	font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.transfer", ChatFormatter.getChatDisplayShort((int) (deriver.getTransfer() / 1000.0 * 20), DisplayUnits.BUCKETS).append(" / s")), 25, 65, 4210752);
-            	font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.linked", deriver.getConnections()), 25, 55, 4210752);
-            	font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.usage", ChatFormatter.getChatDisplayShort(deriver.getTransfer() * 20, DisplayUnits.WATT)), 25, 45, 4210752);
-            	font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.frequency", deriver.getFrequency()), 25, 35, 4210752);
+        	
+        	TileCoercionDeriver deriver = menu.getSafeHost();
+        	
+            if(deriver == null) {
+            	return;
             }
+            
+            font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.transfer", ChatFormatter.getChatDisplayShort((int) (deriver.getTransfer() / 1000.0 * 20), DisplayUnits.BUCKETS).append(" / s")), 25, 65, 4210752);
+        	font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.linked", deriver.getConnections()), 25, 55, 4210752);
+        	font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.usage", ChatFormatter.getChatDisplayShort(deriver.getTransfer() * 20, DisplayUnits.WATT)), 25, 45, 4210752);
+        	font.draw(poseStack, MFFSTextUtils.gui("fortrondevice.frequency", deriver.getFrequency()), 25, 35, 4210752);
         }));
         imageHeight += 40;
         inventoryLabelY += 40;
